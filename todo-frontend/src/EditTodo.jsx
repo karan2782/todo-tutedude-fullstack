b9219478c3e06b681373fb95ca9cb3e7715460bb
karan2782 <karantanwar5005@gmail.com>
2025-11-04 21:11:06 +0530

    updated edit todo

--- todo-frontend/src/EditTodo.jsx
+++ todo-frontend/src/EditTodo.jsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useTodoContext } from "./provider/TodoProvider";
-
 const EditTodo = ({ id, setIsEditing }) => {
   const [title, setTitle] = useState("");
   const [status, setStatus] = useState(false);
   const { fetchTodos } = useTodoContext();
-
   const fetchTodo = async (id) => {
     try {
       const res = await fetch(
@@ -14,78 +12,75 @@ const EditTodo = ({ id, setIsEditing }) => {
       const data = await res.json();
       console.log("edit data:", data);
       if (!res.ok) {
-        alert(data.message || "something went wrong! while fetching todo!");
+        alert(data.message);
       } else {
         setTitle(data.todo.title);
         setStatus(data.todo.completed);
-
       }
     } catch (error) {
-      alert(error.message || "something went wrong! while fetching todo!");
+      alert(error.message);
     }
   };
-
   useEffect(() => {
     fetchTodo(id);
   }, []);
-
   const updateTodo = async (todoData) => {
     try {
       const res = await fetch(
         `https://tutedude-todo-backend.onrender.com/todos/${id}`,
         {
           method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-          },
+          headers: { "Content-Type": "application/json" },
           body: JSON.stringify(todoData),
         }
       );
       const data = await res.json();
       if (!res.ok) {
-        alert(data.message || "something went wrong! while updating todo!");
+        alert(data.message);
       } else {
-        alert("Todo updated successfully!");
+        alert("todo is updated successfully");
         fetchTodos();
         setIsEditing(null);
       }
     } catch (error) {
-      alert(error.message || "something went wrong! while updating todo!");
+      alert(error.message);
     }
   };
-
   const handleSubmit = (e) => {
     e.preventDefault();
     if (title === "") {
-      alert("Title is required");
+      alert("title is require");
       return;
     }
     updateTodo({ title, completed: status ? true : false });
   };
   return (
     <div>
+      {" "}
       <form onSubmit={handleSubmit}>
+        {" "}
         <div className="inputContainer">
-          <label>Title: </label>
+          {" "}
+          <label>title:</label>{" "}
           <input
             type="text"
             placeholder="Enter title"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-          />
-        </div>
+          />{" "}
+        </div>{" "}
         <div className="inputContainer">
-          <label>Completed: </label>
+          {" "}
+          <label>completed:</label>{" "}
           <input
             type="checkbox"
             checked={status}
             onChange={(e) => setStatus(e.target.checked)}
-          />
-        </div>
-        <button>Update Todo</button>
-      </form>
+          />{" "}
+        </div>{" "}
+        <button>update</button>{" "}
+      </form>{" "}
     </div>
   );
 };
-
 export default EditTodo;
